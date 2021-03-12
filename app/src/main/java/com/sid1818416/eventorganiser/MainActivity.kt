9fdb548e19b0f3f2d4f527b0967f62bcb18c1bbc
enasjobs <ernasjobs@gmail.com>
2021-03-12 18:47:19 +0000

    first commit

---
+++ app/src/main/java/com/sid1818416/eventorganiser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sid1818416.eventorganiser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
