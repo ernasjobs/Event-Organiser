880f2c948c3608c15e3e6125e4aadf18f285e586
enasjobs <ernasjobs@gmail.com>
2021-03-14 16:00:30 +0000

    Tweaked few things

---
+++ app/src/main/java/com/sid1818416/eventorganiser/AppNavigator.kt
@@ -0,0 +1,6 @@
+package com.sid1818416.eventorganiser
+
+interface AppNavigator {
+    fun navigateToLogin()
+    fun navigateToRegister()
+}
---
+++ app/src/main/java/com/sid1818416/eventorganiser/Home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.sid1818416.eventorganiser.Home
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.sid1818416.eventorganiser.AppNavigator
+import com.sid1818416.eventorganiser.R
+
+class HomeFragment : Fragment() {
+    private lateinit var appNavigator: AppNavigator
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        appNavigator = context as AppNavigator
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val submitButton: Button = view.findViewById(R.id.button)
+        submitButton.setOnClickListener { appNavigator.navigateToRegister() }
+        val loginLink: TextView = view.findViewById(R.id.loginLink)
+        loginLink.setOnClickListener{appNavigator.navigateToLogin()}
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+}
