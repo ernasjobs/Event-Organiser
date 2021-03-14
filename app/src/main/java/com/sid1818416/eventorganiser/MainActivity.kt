b41c73e5cc3aacb992483a5184ce09ea925ec9be
enasjobs <ernasjobs@gmail.com>
2021-03-14 15:59:38 +0000

    Added onClick listener to login and register button

--- app/src/main/java/com/sid1818416/eventorganiser/MainActivity.kt
+++ app/src/main/java/com/sid1818416/eventorganiser/MainActivity.kt
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.sid1818416.eventorganiser.Home.HomeFragmentDirections
 import com.sid1818416.eventorganiser.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AppNavigator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+    }
+
+    override fun navigateToLogin() {
+        val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+        findNavController(R.id.myNavHostFragment).navigate(action)
+
+    }
+    override fun navigateToRegister() {
+        val action = HomeFragmentDirections.actionHomeFragmentToRegisterFragment()
+        findNavController(R.id.myNavHostFragment).navigate(action)
 
     }
 
